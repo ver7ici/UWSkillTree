7471574d2e273e05d2a73fb53ff659bb7fb90483
ver7ici <gr33ns3v3n@gmail.com>
2022-12-28 13:46:37 -0500

    Update update.py

--- .github/workflows/update.py
+++ .github/workflows/update.py
@@ -1,7 +1,7 @@
 def main():
   print("Hello world")
   with open("wwwroot/sample-data/courses", "w") as f:
-	f.write("hello world")
+    f.write("hello world")
   
 if __name__ == "__main__":
   main()
